d22f5a1a6eb11a795c15008549fb0ea39bc1b07c
Tenzin <tenzin@tenzin.ca>
2021-04-09 16:18:43 -0400

    Fixed templates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,10 @@
 module.exports = {
-  siteMetadata: {
-    title: "Tenzin Namgyal",
-    description: "CV of Tenzin Namgyal",
-    locale: "en",
-  },
   plugins: [
     {
       resolve: "@wkocjan/gatsby-theme-intro",
       options: {
-        basePath: "/",
-        contentPath: "content/",
-        showThemeLogo: true,
         theme: "classic",
       },
     },
-    "gatsby-plugin-sass"
   ],
-};
+}
