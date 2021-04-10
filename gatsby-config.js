2b1cc4c3fe1242cb868418d3c24054152f9f1693
Tenzin <tenzin@tenzin.ca>
2021-04-10 14:10:38 -0400

    Update CV

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,18 @@
 module.exports = {
+  siteMetadata: {
+    description: "Personal page of John Doe",
+    locale: "en",
+    title: "Tenzin Namgyal",
+    formspreeEndpoint: "https://formspree.io/f/xpzkzbwb",
+  },
   plugins: [
     {
       resolve: "@wkocjan/gatsby-theme-intro",
       options: {
-        theme: "classic",
+        basePath: "/",
+        contentPath: "content/",
+        showThemeLogo: true,
+        theme: "bright-green",
       },
     },
   ],
