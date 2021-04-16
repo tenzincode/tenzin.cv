f664cbc113b23783b18251c615d5a76a48c42f08
citizenofearth <namgyal@gmail.com>
2021-04-16 17:02:26 -0400

    Fix CV

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    siteMetadata: {
+        title: "Tenzin Namgyal",
+        description: "CV of Tenzin Namgyal",
+        locale: "en",
+    },
+    plugins: [
+        {
+            resolve: "@wkocjan/gatsby-theme-intro",
+            options: {
+                basePath: "/",
+                contentPath: "content/",
+                showThemeLogo: true,
+                theme: "classic",
+            },
+        },
+    ],
+};
